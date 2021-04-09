19798ad9e89bee9d42e8aa66f948143bbf2c1995
unknown <4Rkhantos>
2021-04-09 11:12:55 -0300

    Nivel god

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -14,7 +14,7 @@ class Resultado extends StatelessWidget{
     }else if(pontuacao < 16){
       return 'Impressionante !';
     }else{
-      return 'Nivel Jedi!';
+      return 'Nivel Transcedente!';
     }
   }
   
